6e91e6f91321108da2f7eba95825511753782441
Checklos <admin@checklos.com>
2022-07-21 19:51:43 +0000

    Update main.go

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	if err := godotenv.Load(); err != nil {
-		log.Printf("Error loading .env with godotenv: %s", err)
-	}
+	//if err := godotenv.Load(); err != nil {
+	//	log.Printf("Error loading .env with godotenv: %s", err)
+	//}
 
 	services.Start()
 }
