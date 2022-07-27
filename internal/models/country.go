75826deb51c092fcdf42ce8191de3cc7da26c6fc
adetunjii <adetunjithomas1@outlook.com>
2022-07-27 10:20:14 +0100

    fix: seed db

--- internal/models/country.go
+++ internal/models/country.go
@@ -1,11 +1,11 @@
 package models
 
 type Country struct {
-	ID   uint   `json:"id" gorm:"primaryKey"`
-	Iso  string `json:"iso"`
-	Name string `json:"name"`
-	//PhoneCode string   `json:"phoneCode"`
-	NiceName string `json:"niceName"`
-	Iso3     string `json:"iso3"`
-	//NumCode   int    `json:"numCode"`
+	ID        uint   `json:"id" gorm:"primaryKey"`
+	Iso       string `json:"iso"`
+	Name      string `json:"name"`
+	PhoneCode int    `json:"phone_code"`
+	NiceName  string `json:"nice_name"`
+	Iso3      string `json:"iso3"`
+	NumCode   int    `json:"num_code"`
 }
--- internal/services/country.go
+++ internal/services/country.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"dh-backend-auth-sv/internal/proto"
 	"encoding/json"
-	"fmt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -22,7 +21,6 @@ func (s *Server) GetAllCountries(ctx context.Context, req *emptypb.Empty) (*prot
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "cannot fetch countries")
 	}
-	fmt.Println(countries)
 	var c []*proto.Country
 
 	err = json.Unmarshal(j, &c)
@@ -30,8 +28,6 @@ func (s *Server) GetAllCountries(ctx context.Context, req *emptypb.Empty) (*prot
 		log.Println(err)
 		return nil, status.Errorf(codes.Internal, "cannot fetch countries")
 	}
-
-	fmt.Println(c)
 
 	res := &proto.GetAllCountryResponse{Countries: c}
 
