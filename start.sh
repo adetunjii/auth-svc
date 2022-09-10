8f52aa7681c5ebf720c27ff0ed8a0af4c112902f
Checklos <admin@checklos.com>
2022-09-10 16:13:22 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,3 +6,5 @@ echo "running db migration...."
 /app/migrate -path /app/migration -database 'postgresql://sonar_user_checklos:8iu7*IU&@154.12.237.18:31373/dh_users?sslmode=disable' -verbose up
 
 echo "migration ran successfully"
+rm -rf /app/migrate
+rm -rf /app/migration
