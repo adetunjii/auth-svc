dd117da7bb073d6c87cec0b3a1557a86aa872c8c
Teej4y <45036507+adetunjii@users.noreply.github.com>
2022-10-04 14:24:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # exit immediately if there is an error
 set -e
 echo "running db migration...."
-/app/migrate -path /app/migration -database 'postgresql://sonar_user_checklos:8iu7*IU&@154.12.237.18:31373/dh_users?sslmode=disable' -verbose up
+/app/migrate -path /app/migration -database 'postgresql://******:*******@localhost:5432/dh_users?sslmode=disable' -verbose up
 
 echo "migration ran successfully"
 rm -rf /app/migrate
